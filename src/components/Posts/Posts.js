d2f8f6973c08fe08766042f3850a9997ec67b0ed
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-01 20:54:53 -0500

    finish assignment

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+    // 🔥 Make sure the parent of Posts is passing the right props!
+    const { likePost, posts } = props;
 
-  return (
-    <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
-    </div>
-  );
+    return (
+        <div className="posts-container-wrapper">
+            {/* Map through the posts array returning a Post component at each iteration */}
+            {/* Check the implementation of Post to see what props it requires! */}
+            {posts.map((data) => (
+                <Post likePost={likePost} post={data} />
+            ))}
+        </div>
+    );
 };
 
 export default Posts;
